67e0d0290cf495a1f3597147345cfda1daa26074
Ruslan Antoshkin <ra.antoshkin@gmail.com>
2019-11-08 00:26:07 +0200

    Added LINQ project

---
+++ 5.LINQ/LINQ/LINQ/LinqExamples/ExtensionMethodSyntax.cs
@@ -0,0 +1,21 @@
+﻿using LINQ.HelpMaterial;
+using System;
+using System.Linq;
+
+namespace LINQ.LinqExamples
+{
+    public static class ExtensionMethodSyntax
+    {
+        public static void ShowExtensionMethodSyntax()
+        {
+            var characters = PersonsService.GetPersons();
+
+            var adults = characters.Where(character => character.Age > 20);
+
+            foreach (var adult in adults)
+            {
+                Console.WriteLine(adult.ToString());
+            }
+        }
+    }
+}
---
+++ 5.LINQ/LINQ/LINQ/Program.cs
@@ -0,0 +1,32 @@
+﻿using LINQ.LinqExamples;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var exampleToRun = ExamplesEnumeration.ExtensionMethodSyntax;
+
+            switch (exampleToRun)
+            {
+                case ExamplesEnumeration.BasicSyntax:
+                    BasicSyntax.ShowBasicSyntaxLinq();
+                    break;
+                case ExamplesEnumeration.GenderCondition:
+                    BasicSyntax.ShowConditionWithGender();
+                    break;
+                case ExamplesEnumeration.ExtensionMethodSyntax:
+                    ExtensionMethodSyntax.ShowExtensionMethodSyntax();
+                    break;
+            }
+
+            Console.Read();
+        }
+    }
+}
