a733ba39c1a196842f4201f5555fafc65fff5420
Ruslan Antoshkin <ra.antoshkin@gmail.com>
2019-11-11 00:31:34 +0200

    Added callback examples

---
+++ 4.DelegatesAndEvents/DelegatesAndEvents/DelegatesAndEvents/DelegatesExamples/BuildInDelegates.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Delegates.DelegatesExamples
+{
+    public static class BuildInDelegates
+    {
+        public static void ShowActionDelegate()
+        {
+            void DestroyEarth() => Console.WriteLine("We are DOOOOOOOOOOOMED!!!");
+
+            Action actionDelegate = new Action(DestroyEarth);
+            //Action actionDelegate = () => Console.WriteLine("We are DOOOOOOOOOOOMED!!!");
+
+            actionDelegate();
+        }
+
+        public static void ShowGenericActionDelegate()
+        {
+            void DestroyPlanet(string planet) => Console.WriteLine($"Message from {planet}: We are DOOOOOOOOOOOMED!!!");
+
+            Action<string> actionDelegate = new Action<string>(DestroyPlanet);
+            //Action actionDelegate = (planet) => Console.WriteLine($"Message from {planet}: We are DOOOOOOOOOOOMED!!!");
+
+            actionDelegate("Solaria");
+        }
+
+        public static void ShowFunctionDelegate()
+        {
+            long DestroyPlanet(string planet) => planet.Equals("Solaria") ? 20000 : 8000000000;
+
+            Func<string, long> funcDelegate = new Func<string, long>(DestroyPlanet);
+            //Func<string, long> funcDelegate = (planet) => planet.Equals("Solaria") ? 20000 : 8000000000;
+            
+            var isPlanetDestroyed = funcDelegate("Solaria");
+            Console.WriteLine($"There are {isPlanetDestroyed} inhabitants were killed :(");
+        }
+
+        public static void ShowPredicateDelegate()
+        {
+            bool TryDestroyPlanet(string planet) => planet.Equals("Solaria") ? true : false;
+
+            Predicate<string> funcDelegate = new Predicate<string>(TryDestroyPlanet);
+            //Predicate<string> funcDelegate = (planet) => planet.Equals("Solaria") ? true : false;
+
+            var isPlanetDestroyed = funcDelegate("Solaria");
+            Console.WriteLine("Is planet destroyed? - " + (isPlanetDestroyed ? "Yes" : "No"));
+        }
+    }
+}
